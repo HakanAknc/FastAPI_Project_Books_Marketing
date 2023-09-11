d5476bcbc2c9bc5921c6c290852d1e915161d281
HakanAknc <hakan62.2001@gmail.com>
2023-09-11 12:24:07 +0300

    geri alındı

--- LoginScreen/login.js
+++ LoginScreen/login.js
@@ -1,32 +1,44 @@
-async function addUser() {
-    const name = document.getElementById("name").value;
-    const surname = document.getElementById("surname").value;
-    const phone = document.getElementById("phone").value;
-    const budget = parseFloat(document.getElementById("budget").value);
+document.addEventListener('DOMContentLoaded', function () {
+    // Formu bulun
+    const form = document.getElementById('myForm');
 
-    const data = {
-        name: name,
-        surname: surname,
-        phone: phone,
-        budget: budget
-    };
+    // Form gönderildiğinde işlem yapın
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Sayfanın yeniden yüklenmesini önler
 
-    try {
-        const response = await fetch('http://127.0.0.1:8000/post_add_users/', {
+        // Formdaki verileri alın
+        const name = document.getElementById('name').value;
+        const surname = document.getElementById('surname').value;
+        const phone = document.getElementById('phone').value;
+        const budget = parseFloat(document.getElementById('budget').value);
+
+        // Verileri bir nesne olarak oluşturun
+        const data = {
+            name: name,
+            surname: surname,
+            phone: phone,
+            budget: budget
+        };
+
+        // POST isteği yapın
+        fetch('http://127.0.0.1:8000/post_add_users/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result => {
+            if (result.message) {
+                alert(result.message);
+                // Başka bir işlem yapmak isterseniz burada yapabilirsiniz
+            } else if (result.error) {
+                alert(result.error);
+            }
+        })
+        .catch(error => {
+            console.error('Hata:', error);
         });
-
-        if (response.ok) {
-            alert('Üye kaydınız başarıyla oluşturuldu.');
-            // Burada isterseniz başka bir işlem yapabilirsiniz.
-        } else {
-            alert('Üye kaydı sırasında bir hata oluştu.');
-        }
-    } catch (error) {
-        console.error('Bir hata oluştu: ', error);
-    }
-}
+    });
+});
